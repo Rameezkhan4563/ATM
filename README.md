5df2b482befa9dffbe3929aba5a3e3d19e9be764
Rameezkhan4563 <160046785+Rameezkhan4563@users.noreply.github.com>
2024-04-03 03:42:23 +0500

    ATM program added

---
+++ README.md
@@ -0,0 +1,2 @@
+# ATM
+ This is a CLI based ATM project
---
+++ main.js
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+let myBalance = 10000;
+let myPin = 1235;
+let answer = await inquirer.prompt([{
+        message: "Enter your pin here",
+        type: "number",
+        name: "Pin"
+    }]);
+if (answer.Pin === myPin) {
+    console.log("Welcome! lets proceed");
+}
+else {
+    console.log("wrong pin");
+}
